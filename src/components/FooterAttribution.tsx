7e5ae90d6f4d6c1984d3e34adf4d9ebeb5ecab71
Arn.28 <arnold.balabarca.27@unsch.edu.pe>
2023-07-02 22:34:52 -0500

    Added footer with attributions

---
+++ src/components/FooterAttribution.tsx
@@ -0,0 +1,15 @@
+import './FooterAttribution.scss'
+
+export const FooterAttribution = () => (
+  <footer className='attribution'>
+    Challenge by{' '}
+    <a href='https://www.frontendmentor.io?ref=challenge' target='_blank'>
+      Frontend Mentor{' '}
+    </a>
+    👨🏻‍💻Coded by{' '}
+    <a target='_blank' href='https://github.com/arn28/'>
+      Arnold Balabarca
+    </a>
+    ⚙️
+  </footer>
+)
