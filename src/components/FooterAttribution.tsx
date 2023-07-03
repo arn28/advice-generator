f1388f1cf6b5cb13eb1435b4ed8e41dc82a248aa
Arn.28 <arnold.balabarca.27@unsch.edu.pe>
2023-07-03 00:55:45 -0500

    Updated icon

--- src/components/FooterAttribution.tsx
+++ src/components/FooterAttribution.tsx
@@ -3,11 +3,11 @@ import './FooterAttribution.scss'
 export const FooterAttribution = () => (
   <footer className='attribution'>
     Challenge by{' '}
-    <a href='https://www.frontendmentor.io?ref=challenge' target='_blank'>
+    <a href='https://www.frontendmentor.io?ref=challenge' target='_blank' rel='noreferrer'>
       Frontend Mentor{' '}
     </a>
     👨🏻‍💻Coded by{' '}
-    <a target='_blank' href='https://github.com/arn28/'>
+    <a target='_blank' href='https://github.com/arn28/' rel='noreferrer'>
       Arnold Balabarca
     </a>
     ⚙️
